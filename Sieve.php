47419a26b638ddac9641421d1d465ff793824b3a
Aleksander Machniak <machniak@kolabsys.com>
2015-12-18 17:52:40 +0100

    Make sure $this->_options is an array to fix possible warning

--- Sieve.php
+++ Sieve.php
@@ -233,7 +233,7 @@ class Net_Sieve
         $this->_sock              = new Net_Socket();
         $this->_bypassAuth        = $bypassAuth;
         $this->_useTLS            = $useTLS;
-        $this->_options           = $options;
+        $this->_options           = (array) $options;
         $this->setDebug($debug, $handler);
 
         /* Try to include the Auth_SASL package.  If the package is not
